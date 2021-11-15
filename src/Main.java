dc9f7be5636440e975c4c0e201b8941e094b7a18
oliver lee <obitlee@msn.com>
2021-11-15 10:24:53 -0500

    Constructed 2 triangles instances.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-
+       Triangle triangleA = new Triangle(15,8, 15,8,17);
+       double Area1 = triangleA.findArea();
+       System.out.println("AREA1:" + Area1);
+       Triangle triangleB = new Triangle(3,2.598, 3,3,3);
+       System.out.println("AREA2:" + triangleB.findArea());
     }
 
 }
